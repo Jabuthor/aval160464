795c842d27a2c9bb4a683eb0bda77dc3831b8ceb
LGZGOMES <LGZGOMES@WS-LAB08_144.univel.univel.br>
2017-06-12 21:27:34 -0300

    Criação do projeto

---
+++ meusistema/src/main/java/br/univel/classes/Aluno.java
@@ -0,0 +1,29 @@
+package br.univel.classes;
+
+public class Aluno {
+
+	private long id;
+	private int nota;
+	private String nome;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getNota() {
+		return nota;
+	}
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
